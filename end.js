bd33db914c75a6980db9dba01e9226a84740d7a3
Saad <saadnoor711@gmail.com>
2020-04-16 05:06:55 +0200

    Developed Game End Screen

---
+++ end.js
@@ -0,0 +1,14 @@
+const username = document.getElementById('username');
+const saveScoreBtn= document.getElementById('saveScoreBtn');
+const finalScore= document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+finalScore.innerText = mostRecentScore;
+username.addEventListener('keyup', ()=> {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = e => {
+    console.log("Clicked");
+    e.preventDefault();
+};
