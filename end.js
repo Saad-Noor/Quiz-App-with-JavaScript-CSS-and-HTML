9c62fcc06ab04fbd9b9ca1198a42c64a23a09c2c
Saad <saadnoor711@gmail.com>
2020-04-16 06:10:25 +0200

    Added Scores to Local Storage

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById('username');
 const saveScoreBtn= document.getElementById('saveScoreBtn');
 const finalScore= document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores= JSON.parse(localStorage.getItem('highScores'))||[];
+
+const MAX_HIGH_SCORE =5;
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener('keyup', ()=> {
@@ -9,6 +12,20 @@ username.addEventListener('keyup', ()=> {
 });
 
 saveHighScore = e => {
-    console.log("Clicked");
+    console.log('Clicked');
     e.preventDefault();
+
+
+    const score = {
+        score: Math.floor(Math.random() *100),
+        name: username.value
 };
+highScores.push(score);
+highScores.sort((x,y) => y.score - x.score);
+highScores.splice(5);
+localStorage.setItem('highScores',JSON.stringify(highScores));
+window.location.assign("/");
+
+};
+
+
