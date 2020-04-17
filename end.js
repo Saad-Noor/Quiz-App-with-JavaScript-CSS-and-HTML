f16c4fe350f8de4ae1e5afd81d23db4a92215ed7
Saad <saadnoor711@gmail.com>
2020-04-17 08:37:02 +0200

    High Scores Screen with Dynamic Scores from Local stogorage

--- end.js
+++ end.js
@@ -11,7 +11,7 @@ username.addEventListener('keyup', ()=> {
     saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = e => {
+saveHighScore = (e) => {
     console.log('Clicked');
     e.preventDefault();
 
